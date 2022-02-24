206d507b948e3058329d5184989b1b575368f010
abdulrafay672 <86514153+abdulrafay672@users.noreply.github.com>
2022-02-25 01:27:26 +0500

    Add files via upload
    weather

---
+++ weather/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from './Components/weatherapp';
+
+function App() {
+  return (
+   <>
+   <Weather />
+   </>
+  );
+}
+
+export default App;
